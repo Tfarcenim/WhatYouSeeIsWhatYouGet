84c636a4560881b32ea32a51aeddf85041159c7f
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2021-04-01 10:12:05 -0400

    changes

--- src/main/java/tfar/whatyouseeiswhatyouget/Tracker.java
+++ src/main/java/tfar/whatyouseeiswhatyouget/Tracker.java
@@ -1,28 +1,34 @@
 package tfar.whatyouseeiswhatyouget;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
+import net.minecraft.loot.LootParameters;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.ForgeMod;
+
+import java.util.List;
 
 public class Tracker {
 
     private int ticks;
     private BlockPos last;
 
-    public Tracker() {
-    }
-
-
     void update(PlayerEntity player) {
         BlockRayTraceResult result = (BlockRayTraceResult) player.pick(player.getAttributeValue(ForgeMod.REACH_DISTANCE.get()),0,true);
+        BlockPos current = result.getPos();
         if (result.getType() == RayTraceResult.Type.MISS) {
             ticks = 0;
             last = null;
         } else {
-            if (!result.getPos().equals(last)) {
+            if (!current.equals(last)) {
                 ticks = 0;
             } else {
                 ticks++;
@@ -30,11 +36,24 @@ public class Tracker {
 
             if (ticks >= WhatYouSeeIsWhatYouGet.TIME) {
                 World world = player.world;
-                world.destroyBlock(result.getPos(),true,player);
+
+                BlockState state = world.getBlockState(current);
+                LootContext.Builder lootcontext$builder = new LootContext.Builder((ServerWorld)world)
+                        .withRandom(world.rand)
+                        .withParameter(LootParameters.field_237457_g_, Vector3d.copyCentered(current))
+                        .withParameter(LootParameters.TOOL, player.getHeldItemMainhand())
+                        .withNullableParameter(LootParameters.BLOCK_ENTITY, world.getTileEntity(current))
+                        .withNullableParameter(LootParameters.THIS_ENTITY, player);
+                List<ItemStack> drops = state.getDrops(lootcontext$builder);
+
+                drops.forEach(itemStack -> {
+                    if (!player.addItemStackToInventory(itemStack)) {
+                        InventoryHelper.spawnItemStack(world,player.getPosX(),player.getPosY(),player.getPosZ(),itemStack);
+                    }
+                });
                 ticks = 0;
             }
-
-            last = result.getPos().toImmutable();
+            last = current.toImmutable();
         }
     }
 }
